d48d14b8915a57264c0308542d560150ad72b2a9
Danial Raza <danialrazafb@gmail.com>
2023-03-21 14:46:45 +0100

    feat: add string exercise

---
+++ javascript/string/script.js
@@ -0,0 +1,16 @@
+const casing = (frase1, frase2) => {
+    return `${frase1.toLowerCase()} ${frase2.toUpperCase()}`;
+}
+
+const frase1 = prompt("Ingrese una frase");
+const frase2 = prompt("Ingrese otra frase");
+
+document.write(`${casing(frase1, frase2)} <br> <br> <br>`);
+
+const capitlizar = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+    // return string[0].toUpperCase() + string.slice(1);
+}
+
+const palabra = prompt("Ingrese una palabra");
+document.write(capitlizar(palabra));
