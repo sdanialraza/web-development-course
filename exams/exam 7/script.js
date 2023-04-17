169cd376d322b858f54a2a93b1fe12fd3a507669
Danial Raza <danialrazafb@gmail.com>
2023-04-17 14:26:50 +0200

    feat: add seventh exam practical

---
+++ exams/exam 7/script.js
@@ -0,0 +1,16 @@
+const offer = document.querySelector("#offer");
+const poster = document.querySelector(".poster");
+
+offer.addEventListener("click", () => {
+    poster.style.display = "block";
+
+    poster.style.animationPlayState = "running";
+});
+
+poster.addEventListener("click", () => {
+    poster.style.animationPlayState = "paused";
+});
+
+poster.addEventListener("dblclick", () => {
+    poster.style.display = "none";
+});
